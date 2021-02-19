28b7093eb8723bc95953f9ed951aa24909315763
Ilham Muhammad <ilhammrg@gmail.com>
2021-02-19 23:18:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [user, setUser] = useState("");
+  const [users, setUsers] = useState([]);
+  const [photos, setPhotos] = useState([]);
 
-  async function getData() {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/users/1"
-    );
-    const data = await response.json();
-    setUser(data.name);
+  async function getUsers() {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users`, {
+        method: "GET"
+      });
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error({ error });
+    }
+  }
+
+  async function getPhotos() {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos`, {
+        method: "GET"
+      });
+      const data = await response.json();
+      setPhotos(data);
+    } catch (error) {
+      console.error({ error });
+    }
   }
 
   useEffect(() => {
-    getData();
+    getUsers();
+    getPhotos();
   }, []);
 
   return (
     <>
       <h1>Hello world!</h1>
-      <p>{user}</p>
+      <h2>Users</h2>
+      <ul>
+        {users.length !== 0 ?
+          users.map((user) => <li key={user.id}>{user.name}</li>)
+          : null
+        }
+      </ul>
+      <h2>Photos</h2>
+      <ul className="photos-container">
+        {photos.length !== 0 ?
+          photos.map((photo) =>
+            <li className="photo-wrapper" key={photo.url+photo.id}>
+              <img className="photo" src={photo.url} alt={photo.title} />
+            </li>
+          )
+          : null
+        }
+      </ul>
     </>
   );
 };
